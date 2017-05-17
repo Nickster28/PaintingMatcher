e0072e7ec45f9bf4ade70545557f9b91e8cae7be
Nick Troccoli <nicktroccoli28@gmail.com>
2017-05-17 00:28:34 -0700

    added mini dataset

--- dataset/processing.py
+++ dataset/processing.py
@@ -1,6 +1,7 @@
 import csv
 import urllib
 from PIL import Image
+import random
 
 # Outputs the number of paintings under each theme for the given dataset file
 def outputThemeCounts(dataset, output):
@@ -43,36 +44,40 @@ def outputMalformedRows(dataset):
             if not row[6].startswith("https://"):
                 print(counter)
 
-def downloadImages(dataset, output):
+def downloadImages(dataset, output, thumbnailSize, count=None):
     with open(dataset, 'rb') as datasetFile:
         with open(output, 'wb') as outputFile:
             writer = csv.writer(outputFile, delimiter=',')
             reader = csv.reader(datasetFile, delimiter=',')
 
             counter = 0
-            for row in reader:
-                counter += 1
-                if counter == 1:
-                    writer.writerow(row + ['Filename'])
-                    continue
+            rows = [row for row in reader if row[6].startswith("https://")]
+            writer.writerow(rows[0] + ['Filename'])
+            rows = rows[1:]
 
-                if not row[6].startswith("https://"): continue
-                filename = str(counter) + ".jpg"
+            if count == None: count = len(rows)
+
+            while count > 0:
+                index = random.randint(0, len(rows)) # Pick random index
+                row = rows[index]
+                filename = str(index) + ".jpg"
 
                 try:
                     urllib.urlretrieve(row[6], "images/" + filename)
                     img = Image.open("images/" + filename)
-                    img = img.resize((300,300))
+                    img = img.resize(thumbnailSize)
                     img.save("images/" + filename)
                     writer.writerow(row + [filename])
-                    print("(" + str(counter) + " of " + str(35748) + ") - " + row[1])
+                    print(str(count) + " - " + row[1])
+                    del rows[index]
+                    count -= 1
                 except Exception as e:
                     print(row[1] + " - error: " + str(e))
 
 
 #outputThemeCounts('dataset.csv', 'theme-counts.csv')
 #outputMalformedRows('dataset.csv')
-downloadImages('dataset.csv', 'datset-new.csv')
+downloadImages('dataset.csv', 'datset-new.csv', (200, 200), count=100)
 
 
 
