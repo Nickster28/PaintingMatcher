88903d7c5b70bf8d4197d02543172bcb8db66867
Nick Troccoli <nicktroccoli28@gmail.com>
2017-05-16 22:27:09 -0700

    Updated dataset file

--- dataset/processing.py
+++ dataset/processing.py
@@ -1,42 +1,30 @@
 import csv
 
-def outputDict(theme):
-	with open('output.csv', 'wb') as csvfile:
-		writer = csv.writer(csvfile, delimiter=',')
+# Outputs the number of paintings under each theme for the given dataset file
+def outputThemeCounts(dataset, output):
+    with open(dataset, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        counter = 0
+        theme_dict = {}
 
-		# Theme
-		writer.writerow(["Themes"])
-		for key in theme:
-			key = unicode(key, "utf-8")
-			writer.writerow([key, theme[key]])
+        for row in reader:
+            counter += 1;
+            if counter == 1: continue
 
+            # Theme
+            if row[0] in theme_dict:
+                theme_dict[row[0]] += 1
+            else:
+                theme_dict[row[0]] = 1
 
-with open('by-topic.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    counter = 0
-    
-    startIndex = -1
-    length = 0
+        # Output theme counts
+        with open(output, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
 
-    theme_dict = {}
-    for row in reader:
-    	counter += 1;
-        if counter == 1: continue
+            # Theme
+            writer.writerow(["Themes"])
+            for key in theme_dict:
+                key = unicode(key, "utf-8")
+                writer.writerow([key, theme_dict[key]])
 
-        # Theme
-        if row[0] in theme_dict:
-        	theme_dict[row[0]] += 1
-        else:
-        	theme_dict[row[0]] = 1
-
-        if not row[5].startswith("https://") or not row[6].startswith("https://"):
-            if startIndex == -1:
-                startIndex = counter
-            elif counter == startIndex + length + 1:
-                length += 1
-            elif startIndex != -1:
-                print(str(startIndex) + " to " + str(startIndex + length))
-                startindex = counter
-                length = 1
-
-    outputDict(theme_dict)
+outputThemeCounts('dataset.csv', 'theme-counts.csv')
