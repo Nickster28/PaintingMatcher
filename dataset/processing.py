c69b23494c939705e048bbe2ab1efb48d758f7b4
Nick Troccoli <nicktroccoli28@gmail.com>
2017-05-16 23:42:23 -0700

    Added image download script

--- dataset/processing.py
+++ dataset/processing.py
@@ -1,4 +1,6 @@
 import csv
+import urllib
+from PIL import Image
 
 # Outputs the number of paintings under each theme for the given dataset file
 def outputThemeCounts(dataset, output):
@@ -41,7 +43,37 @@ def outputMalformedRows(dataset):
             if not row[6].startswith("https://"):
                 print(counter)
 
+def downloadImages(dataset, output):
+    with open(dataset, 'rb') as datasetFile:
+        with open(output, 'wb') as outputFile:
+            writer = csv.writer(outputFile, delimiter=',')
+            reader = csv.reader(datasetFile, delimiter=',')
+
+            counter = 0
+            for row in reader:
+                counter += 1
+                if counter == 1:
+                    writer.writerow(row + ['Filename'])
+                    continue
+
+                if not row[6].startswith("https://"): continue
+                filename = str(counter) + ".jpg"
+
+                try:
+                    urllib.urlretrieve(row[6], "images/" + filename)
+                    img = Image.open("images/" + filename)
+                    img = img.resize((300,300))
+                    img.save("images/" + filename)
+                    writer.writerow(row + [filename])
+                    print("(" + str(counter) + " of " + str(35748) + ") - " + row[1])
+                except Exception as e:
+                    print(row[1] + " - error: " + str(e))
+
+
 #outputThemeCounts('dataset.csv', 'theme-counts.csv')
-outputMalformedRows('dataset.csv')
+#outputMalformedRows('dataset.csv')
+downloadImages('dataset.csv', 'datset-new.csv')
+
+
 
 
