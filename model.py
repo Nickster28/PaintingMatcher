f5a3a155bd529b23b90490bbd90feb1375affad3
Christina Hung <cjh@cs.stanford.edu>
2017-05-17 00:17:57 -0700

    tf skeleton

---
+++ model.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+import numpy as np
+import math
+import timeit
+import matplotlib.pyplot as plt
+
+from cs231n.data_utils import load_CIFAR10
+
+def get_art_data(num_training=49000, num_validation=1000, num_test=10000):
+    # get art data, divide into train, valid, test
+    pass
+
+def run_model(session, predict, loss_val, Xd, yd,
+              epochs=1, batch_size=64, print_every=100,
+              training=None, plot_losses=False):
+    # have tensorflow compute accuracy
+    correct_prediction = tf.equal(tf.argmax(predict,1), y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+    # shuffle indicies
+    train_indicies = np.arange(Xd.shape[0])
+    np.random.shuffle(train_indicies)
+
+    training_now = training is not None
+    
+    # setting up variables we want to compute (and optimizing)
+    # if we have a training function, add that to things we compute
+    variables = [mean_loss,correct_prediction,accuracy]
+    if training_now:
+        variables[-1] = training
+    
+    # counter 
+    iter_cnt = 0
+    for e in range(epochs):
+        # keep track of losses and accuracy
+        correct = 0
+        losses = []
+        # make sure we iterate over the dataset once
+        for i in range(int(math.ceil(Xd.shape[0]/batch_size))):
+            # generate indicies for the batch
+            start_idx = (i*batch_size)%X_train.shape[0]
+            idx = train_indicies[start_idx:start_idx+batch_size]
+            
+            # create a feed dictionary for this batch
+            feed_dict = {X: Xd[idx,:],
+                         y: yd[idx],
+                         is_training: training_now }
+            # get batch size
+            actual_batch_size = yd[i:i+batch_size].shape[0]
+            
+            # have tensorflow compute loss and correct predictions
+            # and (if given) perform a training step
+            loss, corr, _ = session.run(variables,feed_dict=feed_dict)
+            
+            # aggregate performance stats
+            losses.append(loss*actual_batch_size)
+            correct += np.sum(corr)
+            
+            # print every now and then
+            if training_now and (iter_cnt % print_every) == 0:
+                print("Iteration {0}: with minibatch training loss = {1:.3g} and accuracy of {2:.2g}"\
+                      .format(iter_cnt,loss,np.sum(corr)/actual_batch_size))
+            iter_cnt += 1
+        total_correct = correct/Xd.shape[0]
+        total_loss = np.sum(losses)/Xd.shape[0]
+        print("Epoch {2}, Overall loss = {0:.3g} and accuracy of {1:.3g}"\
+              .format(total_loss,total_correct,e+1))
+        if plot_losses:
+            plt.plot(losses)
+            plt.grid(True)
+            plt.title('Epoch {} Loss'.format(e+1))
+            plt.xlabel('minibatch number')
+            plt.ylabel('minibatch loss')
+            plt.show()
+    return total_loss,total_correct
+
+with tf.Session() as sess:
+    with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0" 
+        sess.run(tf.global_variables_initializer())
+        print('Training')
+        run_model(sess,y_out,mean_loss,X_train,y_train,1,64,100,train_step,True)
+        print('Validation')
+        run_model(sess,y_out,mean_loss,X_val,y_val,1,64)
+# clear old variables
+tf.reset_default_graph()
+
+# define our input (e.g. the data that changes every batch)
+# The first dim is None, and gets sets automatically based on batch size fed in
+X = tf.placeholder(tf.float32, [None, 200, 200, 3])
+y = tf.placeholder(tf.int64, [None])
+is_training = tf.placeholder(tf.bool)
+
+# define model
+def model(X,y,is_training):
+    # define our weights (e.g. init_two_layer_convnet)
+    # setup variables
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 5])
+    bconv1 = tf.get_variable("bconv1", shape=[5])
+    W1 = tf.get_variable("W1", shape=[50000, 1024])
+    b1 = tf.get_variable("b1", shape=[1024])
+    W2 = tf.get_variable("W2", shape=[1024, 2])
+    b2 = tf.get_variable("b2", shape=[2])
+
+    # define our graph (e.g. two_layer_convnet)
+    y_out = X
+    y_out = tf.nn.conv2d(X, Wconv1, strides=[1,1,1,1], padding='SAME') + bconv1
+    y_out = tf.nn.relu(y_out)
+    y_out = tf.layers.batch_normalization(y_out, axis=3, training=is_training)
+    y_out = tf.layers.max_pooling2d(y_out, 2, 2, padding='SAME')
+    y_out = tf.reshape(y_out,[-1, 50000])
+    y_out = tf.matmul(y_out, W1) + b1
+    y_out = tf.nn.relu(y_out)
+    y_out = tf.matmul(y_out, W2) + b2
+    return y_out
+
+y_out = model(X,y,is_training)
+
+# Inputs
+#     y_out: is what your model computes
+#     y: is your TensorFlow variable with label information
+# Outputs
+#    mean_loss: a TensorFlow variable (scalar) with numerical loss
+#    optimizer: a TensorFlow optimizer
+# This should be ~3 lines of code!
+mean_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_out, labels=tf.one_hot(y, 10)))
+optimizer = tf.train.RMSPropOptimizer(1e-3)
+pass
+
+# batch normalization in tensorflow requires this extra dependency
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(extra_update_ops):
+    train_step = optimizer.minimize(mean_loss)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+print('Training')
+run_model(sess,y_out,mean_loss,X_train,y_train,1,64,100,train_step)
+
+print('Validation')
+run_model(sess,y_out,mean_loss,X_val,y_val,1,64)
